5812bdc03570119b0d5424089b47dac3ff0c4f86
takashiii <azufa.fish@gmail.com>
2020-04-18 00:30:06 +0900

    define a board of o-x game

---
+++ board.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# Board class expresses the marubatsu game's board.
+class Board
+  attr_reader :board
+
+  def initialize(board_size: 3)
+    @board_size = board_size
+    @board = board_size.times.map do
+      board_size.times.map { |_val| '.' }
+    end
+  end
+
+  def put_mark(mark:, board_x:, board_y:)
+    if board_x.negative? || board_x >= @board_size
+      raise ArgumentError, "board_x must be between 0 to #{@board_size - 1}"
+    end
+    if board_y.negative? || board_y >= @board_size
+      raise ArgumentError, "board_y must be between 0 to #{@board_size - 1}"
+    end
+
+    @board[board_y][board_x] = mark
+  end
+
+  def print_board
+    puts '┏━┳━┳━┓'
+    @board.each_with_index do |line, index|
+      formatted_line = '┃'
+      formatted_line += line.map do |square|
+        "#{square}┃"
+      end.join
+      puts formatted_line
+      puts '┣━╋━╋━┫' if index < @board.size - 1
+      puts '┗━┻━┻━┛' if index == @board.size - 1
+    end
+  end
+end
