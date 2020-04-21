8d8a379d40be8b50715b271e3d79d813d3a5b6f8
takashiii <azufa.fish@gmail.com>
2020-04-22 00:09:27 +0900

    count turn when put a mark

--- player.rb
+++ player.rb
@@ -1,12 +1,14 @@
 class Player
   attr_reader :mark
 
-  def initialize(mark:, board:)
+  def initialize(mark:, board:, game_state:)
     @mark = mark
     @board = board
+    @game_state = game_state
   end
 
   def put(x:, y:)
     @board.put_mark(mark: @mark, board_x: x, board_y: y)
+    @game_state.proceed_turn
   end
 end
